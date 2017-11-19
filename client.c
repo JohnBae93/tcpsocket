eb4f74ff94392140d42512f637ec47bf827ccb88
John <johnbae57@naver.com>
2017-11-19 13:17:00 +0900

    check basic send and receive

---
+++ client.c
@@ -0,0 +1,74 @@
+/* tcpclient.c */
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#define BYTE_MAX 1024
+#define PORT 5000
+
+int main() {
+    int sock, bytes_recieved;
+    char recv_data[BYTE_MAX];
+    struct hostent *host;
+    struct sockaddr_in server_addr;
+
+    char *fname = "task.txt";
+    string line;
+
+    /* [1] Open input file */
+    ifstrem infile(fname);
+    if (infile.isopen) {
+        cout << "[Clear] Success to open " << fname << endl;
+
+        /* [2] Create socket*/
+        host = gethostbyname("127.0.0.1");
+
+        if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+            perror("[Error] Fail to create socket\n");
+            exit(1);
+        }
+
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port = htons(PORT);
+        server_addr.sin_addr = *((struct in_addr *) host->h_addr);
+        bzero(&(server_addr.sin_zero), 8);
+
+        if (connect(sock, (struct sockaddr *) &server_addr,
+                    sizeof(struct sockaddr)) == -1) {
+            perror("[Error] Fail to connect server");
+            exit(1);
+        }
+
+        /* [3] Read file line by line*/
+        while (getline(infile, line)) {
+            cout << "[Clear] Read and send \"" << line << "\"" << endl;
+            send(sock, line, line.length());
+
+            /* */
+            cout << "[Receive]" << endl;
+            while (bytes_recieved = recv(sock, recv_data, BYTE_MAX, 0) > 0) {
+                recv_data[bytes_recieved] = '\0';
+                cout << recv_data << endl;
+            }
+        }
+
+
+    } else {
+        cout << "[Error] Fail to open " << fname << " in directory" << endl;
+        exit(1);
+    }
+
+    infile.close();
+    close(sock);
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,87 @@
+/* tcpserver.c */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#define BYTE_MAX 1024
+#define PORT 5000
+
+void readProducts(char *fname, sock) {
+
+}
+
+int main() {
+    int lsock, sock, bytes_recieved, True = 1;
+    char send_data[BYTE_MAX], recv_data[BYTE_MAX];
+
+    struct sockaddr_in server_addr, client_addr;
+    int sin_size;
+
+    char *pfile = "products.txt";
+    char *ofile = "orders.txt";
+
+    /*[1] Create listen socket*/
+    if ((lsock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("[Error] Fail to create socket\n");
+        exit(1);
+    }
+
+    if (setsockopt(lsock, SOL_SOCKET, SO_REUSEADDR, &True, sizeof(int)) == -1) {
+        perror("[Error] Fail to set sock option");
+        exit(1);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    bzero(&(server_addr.sin_zero), 8);
+
+    if (bind(lsock, (struct sockaddr *) &server_addr, sizeof(struct sockaddr))
+        == -1) {
+        perror("[Error] Fail to bind");
+        exit(1);
+    }
+
+    if (listen(lsock, 5) == -1) {
+        perror("[Error] Fail to Listen");
+        exit(1);
+    }
+
+    cout << "TCPServer Waiting for client on port" << PORT << endl;
+    fflush(stdout);
+
+
+    while (1) {
+
+        sin_size = sizeof(struct sockaddr_in);
+        sock = accept(lsock, (struct sockaddr *) &client_addr, &sin_size);
+
+        cout << "I got a connection from (" << inet_ntoa(client_addr.sin_addr)
+             << " , " << ntohs(client_addr.sin_port) << endl;
+
+        while (1) {
+            bytes_recieved = recv(connected, recv_data, BYTE_MAX, 0);
+            recv_data[bytes_recieved] = '\0';
+
+            cout << "[Receive] " << recv_data << endl;
+            fflush(stdout);
+            if(recv_data == '5')
+                break;
+
+        }
+    }
+
+    close(lsock);
+    close(sock);
+    return 0;
+}
